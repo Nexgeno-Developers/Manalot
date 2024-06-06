ceab47959b26eda3f78a8d4d89d97ac062754a38
junaid53 <ansarijunaid53@gmial.com>
2024-06-06 15:12:43 +0530

    micro manage

---
+++ resources/views/backend/pages/experience_status/index.blade.php
@@ -0,0 +1,71 @@
+@extends('backend.layouts.app')
+
+@section('page.name', 'Experience')
+
+@section('page.content')
+<div class="card">
+    <div class="card-body">
+        <div class="row mb-2">
+            <div class="col-sm-5">
+                <h3>List</h3>
+            </div>
+            <div class="col-sm-7">
+                <div class="text-sm-end">
+                    <a href="javascript:void(0);" class="btn btn-danger mb-2"
+                        onclick="smallModal('{{ url(route('manage.add_experience_status')) }}', 'Add Experience')"><i
+                            class="mdi mdi-plus-circle me-2"></i> Add Experience</a>
+                </div>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table id="basic-datatable5" class="table dt-responsive nowrap w-100">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($experience_statuses as $status)
+                    <tr>
+                        <td>{{ $status->id }}</td>
+                        <td>{{ $status->name }}</td>
+                        <td>{{ $status->status }}</td>
+                        <td>
+                            <a href="javascript:void(0);" class="btn btn-info text-white action-icon"
+                                onclick="smallModal('{{ url(route('manage.edit_experience_status',['id' => $status->id])) }}', 'Edit user')">
+                                <i class="mdi mdi-square-edit-outline" title="Edit"></i>
+                            </a>
+                            {{-- <a href="javascript:void(0);" class="btn btn-danger text-white action-icon"
+                                onclick="deleteExperienceStatus('{{ url(route('manage.delete_experience_status', $status->id)) }}')">
+                            <i class="mdi mdi-delete" title="Delete"></i>
+                            </a> --}}
+                            <a href="javascript:void(0);" class="btn btn-danger text-white action-icon" onclick="confirmModal('{{ url(route('manage.delete_experience_status', $status->id)) }}',
+                            responseHandler)">
+                                <i class="mdi mdi-delete" title="Delete"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section("page.scripts")
+<script>
+$(document).ready(function() {
+    var table = $('#basic-datatable5').DataTable();
+});
+</script>
+
+<script>
+var responseHandler = function(response) {
+    location.reload();
+}
+</script>
+@endsection
