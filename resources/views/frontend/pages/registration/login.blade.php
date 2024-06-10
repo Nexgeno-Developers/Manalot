75e2a70e5c89f54037a56a454181abcbd9305000
Mahtab Alam <mehtab.makent@gmail.com>
2024-06-10 16:57:13 +0530

    footer add

--- resources/views/frontend/pages/registration/login.blade.php
+++ resources/views/frontend/pages/registration/login.blade.php
@@ -16,7 +16,7 @@
                 <img src="/assets/images/login_bg.png" style="width:100%">
             </div>
             <div class="col-md-6 pddleft50">
-                <h1 class="main_heading mb-5">
+                <h1 class="main_heading mt50">
                     <span class="purple font-size70">M</span>analot
                     <span class="orange font-size70">L</span>eadership
                     <span class="aquamarine font-size70">N</span>etwork
@@ -89,7 +89,7 @@
 
 
 
-<section>
+<section class="pt-5">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-6 width55">
@@ -126,7 +126,76 @@
         </div>
     </div>
 </section>
-<footer class="text-center pt-4 pb-4"><b>© Maple Consulting and Services</b></footer>
+<footer class="ms-footer">
+	<div class="ms-footer-wrap">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-6 col-lg-3">
+					 <div class="ms-footer-col-1">
+						<div class="ms-footer-logo-imgbox">
+							<img loading="lazy" src="https://manalot.com/public/gfx/logo-manalot-footer.png" alt="manalot-logo">
+						</div>
+						<div class="ms-footer-address">
+							<div class="ms-footer-heading">
+								<h2>CONTACT INFO</h2>
+							</div>
+							<div class="ms-address-row">
+								<span class="ms-address-ui">
+									<i class="fa-solid fa-regular fa-location-dot" aria-hidden="true"></i>
+								</span>
+								<address class="ms-address-content">
+									<span class="ms-ad-heading">Address:</span>Malhotra Chambers, 401, 4th Floor, Off. Govandi Station Road, Behind USV Ltd., Deonar, Chembur, Mumbai - 400 088
+								</address>
+							</div>
+							<div class="ms-address-row">
+								<span class="ms-address-ui">
+									<i class="fa-regular fa-envelope" aria-hidden="true"></i>
+								</span>
+								<div class="ms-footer-email">
+								<span class="ms-ad-heading">Email:</span>
+								<a class="ms-address-content" href="mailto:communications@manalot.com">communications@manalot.com</a>
+								</div>
+							</div>
+						</div>
+					</div>				</div>
+				<div class="col-12 col-md-6 col-lg-3">
+					<div class="ms-footer-heading">
+						<h2>SERVICES</h2>
+					</div>
+					<ul class="ms-footer-services">
+						<li><a href="https://manalot.com/talent-selection" class="ms-footer-services-link">Talent Selection</a></li>
+						<li><a href="https://manalot.com/manalot" class="ms-footer-services-link">Manalot – AI Powered Executive Search</a></li>
+						<li><a href="https://manalot.com/talent-strategy-organizational-alignment" class="ms-footer-services-link">Talent Strategy &amp; Organizational Alignment</a></li>
+						<li><a href="https://manalot.com/hr-advanced-analytics" class="ms-footer-services-link">HR Advanced Analytics</a></li>
+						<li><a href="https://manalot.com/aspirational-modelling" class="ms-footer-services-link">Aspirational Modelling</a></li>
+						<li><a href="https://manalot.com/family-business-advisory" class="ms-footer-services-link">Family Business Advisory</a></li>
+						<li><a href="https://manalot.com/professional-mentoring-career-consulting" class="ms-footer-services-link">Professional Mentoring &amp; Career Consulting</a></li>
+					</ul>				</div>
+				<div class="col-12 col-md-6 col-lg-3">
+					<div class="ms-footer-heading">
+						<h2>USEFUL LINKS</h2>
+					</div>
+					<ul class="ms-footer-services">
+						<li><a href="https://manalot.com/" class="ms-footer-services-link">Home</a></li>
+						<li><a href="https://manalot.com/leadership-team" class="ms-footer-services-link">Who We Are</a></li>
+
+<li><a href="https://manalot.com/careers" class="ms-footer-services-link">Careers</a></li>
+						<li><a href="https://manalot.com/contact-us" class="ms-footer-services-link">Contact Us</a></li>
+
+<li><a href="https://manalot.com/privacy-policy" class="ms-footer-services-link">Privacy Policy</a></li>
+					</ul>				</div>
+				<div class="col-12 col-md-6 col-lg-3">
+					<div class="ms-footer-heading">
+						<h2>FOLLOW US</h2>
+					</div>
+					<div class="ms-footer-social">
+						<a href="https://www.linkedin.com/company/manalot/" target="_blank" class="ms-social-link linkdin" aria-label="Linkdin"><i class="fa-brands fa-linkedin" aria-hidden="true"></i></a>
+					</div>				</div>
+			</div>
+		</div>
+	</div>
+	<div class="ms-footer-copyright"><p>© Copyright - Manalot.</p></div>
+</footer>
 
 <!----------========================== login ============----------->
 @endsection
